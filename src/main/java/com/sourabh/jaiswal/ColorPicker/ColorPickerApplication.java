deb35ceff8b7d9c8e4a3218121da62c3facf9394
sourabh8jaiswal <sourabh121jaiswal@gmail.com>
2021-06-15 18:41:59 +0530

    color picker

---
+++ src/main/java/com/sourabh/jaiswal/ColorPicker/ColorPickerApplication.java
@@ -0,0 +1,13 @@
+package com.sourabh.jaiswal.ColorPicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorPickerApplication.class, args);
+	}
+
+}
